4f57c3c86993455c6f0a196c7710faefbada6c5a
Ali Yusein <>
2023-10-16 14:03:24 +0200

    no message

--- src/mappers/historyMappers.ts
+++ src/mappers/historyMappers.ts
@@ -3,18 +3,19 @@ import BlockMeta from "../types/BlockMeta"
 import History from "../types/History"
 import HistoryPart from "../types/HistoryPart"
 
+const energyperbyteinkwh = parseFloat(process.env.energyperbyteinkwh || '4.56')
+
 interface blockMetaExt { 
     hash: string, 
     time: number 
 }
 
-const energyperbyteinkwh = parseFloat(process.env.energyperbyteinkwh || '4.56')
+const sumHistoryPartEnergy = (arr: Array<HistoryPart>): number => {
+    const rawSum: number = arr.reduce((acc, curr) => acc + (curr.energyInKwH * 1000), 0) // energyInKwH is already calculated
+    return rawSum / 1000
+}
 
 const mapHistory = (historyParts: Array<HistoryPart>, days: number) => {
-    const sumHistoryPartEnergy = (arr: Array<HistoryPart>): number => {
-        const rawSum: number = arr.reduce((acc, curr) => acc + (curr.energyInKwH * 1000), 0) // energyInKwH is already calculated
-        return rawSum / 1000
-    }
     const history: History = {
         periodInDays: days,
         energyInKwH: sumHistoryPartEnergy(historyParts),
@@ -23,12 +24,12 @@ const mapHistory = (historyParts: Array<HistoryPart>, days: number) => {
     return history
 }
 
+
+const sumBlocksEnergy = (arr: Array<Block>): number => {
+    const rawSum: number = arr.reduce((acc, curr) => acc + (curr.size * (energyperbyteinkwh * 1000)), 0)
+    return rawSum / 1000
+}
 const mapHistoryPart = (blockDataForCalendarDay: Array<Block>, dayNo: number): HistoryPart => {
-    const sumBlocksEnergy = (arr: Array<Block>): number => {
-        const rawSum: number = arr.reduce((acc, curr) => acc + (curr.size * (energyperbyteinkwh * 1000)), 0)
-        return rawSum / 1000
-    }
-    
     const historyPart: HistoryPart = {
         day: dayNo,
         energyInKwH: sumBlocksEnergy(blockDataForCalendarDay),
--- src/resolvers/walletResolvers.ts
+++ src/resolvers/walletResolvers.ts
@@ -11,25 +11,49 @@ const walletCache = 60
 const defaultLimit = 20
 const rateLimitInSeconds = 10
 
-const getWalledDataWithPagination = async (address: string): Promise<AxiosResponse> => {
-    let offset: number = 0
-    const walletResponse: AxiosResponse = await client.get(`/rawaddr/${address}?limit=${defaultLimit}`)
-    const numberOfBatches: number = Math.ceil((walletResponse.data).n_tx / defaultLimit) - 1 // firs call already made above
-    const promises: Array<Promise<AxiosResponse>> = []
-    for(let i = 0; i < numberOfBatches; i++) {
-        offset += defaultLimit
-        logger.debug({message: `Waiting ${rateLimitInSeconds} seconds for rate limit`})
-        await utils.delay(rateLimitInSeconds * 1000)
-        logger.debug({message: `calling /rawaddr/${address}?limit=${defaultLimit}&offset=${offset}`})
-        promises.push(client.get(`/rawaddr/${address}?limit=${defaultLimit}&offset=${offset}`))
-        logger.debug({message: `Got wallet txs part ${i+1}/${numberOfBatches + 1}`})
+const callWalletApiWithPagination = (address:string, limit: number, offset: number): Promise<AxiosResponse>  => client.get(`/rawaddr/${address}?limit=${limit}&offset=${offset}`)
+
+const collectWalletTxs = async (address:string, limit: number, offset: number, result: AxiosResponse): Promise<AxiosResponse> => {
+    if(result.data.n_tx <= (offset + limit)) {
+        return result
     }
-    const promiseResponses: Array<AxiosResponse> = await Promise.all(promises)
-    const walletResponseWithTxsPagination: AxiosResponse = walletResponse
-    promiseResponses.forEach((response):AxiosResponse => walletResponseWithTxsPagination.data.txs.push(...response.data.txs) )
-    return walletResponseWithTxsPagination
+    
+    logger.debug({message: `Waiting ${rateLimitInSeconds} seconds for rate limit`})
+    await utils.delay(rateLimitInSeconds * 1000)
+
+    offset += limit
+    logger.debug({message: `calling /rawaddr/${address}?limit=${limit}&offset=${offset}`})
+    const response: AxiosResponse = await callWalletApiWithPagination(address, limit, offset)
+
+    result.data.txs.push(...response.data.txs)
+
+    return await collectWalletTxs(address, limit, offset, result)
 }
 
+const getWalletData = async (address: string, offset:number, limit:number): Promise<AxiosResponse> => {
+    logger.debug({message: `calling /rawaddr/${address}?limit=${limit}&offset=${offset}`})
+    const walletResponse: AxiosResponse = await callWalletApiWithPagination(address, limit, offset)
+    return await collectWalletTxs(address, limit, offset, walletResponse)
+}
+
+// const getWalledDataWithPagination = async (address: string): Promise<AxiosResponse> => {
+//     let offset: number = 0
+//     const walletResponse: AxiosResponse = await client.get(`/rawaddr/${address}?limit=${defaultLimit}`)
+//     const numberOfBatches: number = Math.ceil((walletResponse.data).n_tx / defaultLimit) - 1 // firs call already made above
+//     const promises: Array<Promise<AxiosResponse>> = []
+//     for(let i = 0; i < numberOfBatches; i++) {
+//         offset += defaultLimit
+//         logger.debug({message: `Waiting ${rateLimitInSeconds} seconds for rate limit`})
+//         await utils.delay(rateLimitInSeconds * 1000)
+//         logger.debug({message: `calling /rawaddr/${address}?limit=${defaultLimit}&offset=${offset}`})
+//         promises.push(client.get(`/rawaddr/${address}?limit=${defaultLimit}&offset=${offset}`))
+//         logger.debug({message: `Got wallet txs part ${i+1}/${numberOfBatches + 1}`})
+//     }
+//     const promiseResponses: Array<AxiosResponse> = await Promise.all(promises)
+//     const walletResponseWithTxsPagination: AxiosResponse = walletResponse
+//     promiseResponses.forEach((response):AxiosResponse => walletResponseWithTxsPagination.data.txs.push(...response.data.txs) )
+//     return walletResponseWithTxsPagination
+// }
 
 const walletResolvers = {
     getWallet: async (address: string): Promise<Wallet> => {
@@ -44,7 +68,9 @@ const walletResolvers = {
             }
 
             logger.debug({ message: `FETCH HIT! -> ${address}` })
-            const walletResponse: AxiosResponse = await getWalledDataWithPagination(address)
+            const walletResponse: AxiosResponse = await getWalletData(address, 0, defaultLimit)
+            // const walletResponse: AxiosResponse = await getWalledDataWithPagination(address)
+            
             const wallet: Wallet = mapWallet(walletResponse.data);
 
             await redis.setex(cacheKey, walletCache, JSON.stringify(wallet));
